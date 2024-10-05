0bb37220fb6d0a8d85af449cdef6b650fa489ca1
layituan-tihom <142542198+mnohit98@users.noreply.github.com>
2024-10-05 05:43:59 +0530

    updating base url

--- config/db.config.js
+++ config/db.config.js
@@ -15,7 +15,7 @@ connection.connect((err) => {
         console.error('Error connecting to the database:', err.stack);
         return;
     }
-    console.log('Connected to the database.');
+    console.log('Connected to the database as ID', connection.threadId);
 });
 
 module.exports = connection;
--- swagger.js
+++ swagger.js
@@ -13,7 +13,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:3000', // Server URL
+      url: process.env.BASE_URL || 'http://localhost:3000', // Server URL
     },
   ],
 };
